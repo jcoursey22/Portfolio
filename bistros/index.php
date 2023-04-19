5382309479d04f62e0eb731086b5522862f0c86a
jcoursey22 <101881553+jcoursey22@users.noreply.github.com>
2023-04-19 10:30:33 -0400

    Add files via upload

---
+++ bistros/index.php
@@ -0,0 +1,144 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta name="description" content="">
+<meta name="author" content="">
+<title>Bistros | Jacksonville</title>
+<!-- Bootstrap core CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<!-- Custom styles for this template -->
+</head>
+
+<body style="background-image: url(redbrick.jpg);background-repeat: repeat;background-attachment:fixed;">
+
+<!-- Bootstrap core JavaScript -->
+<script src="vendor/jquery/jquery.min.js"></script>
+<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
+
+<!-- Navigation -->
+<nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top container">
+  <div class="container">
+	<a class="navbar-brand" href="#">BISTROS <span style="font-family: 'Pacifico', cursive;">Jacksonville, FL</span></a>
+	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+	  <span class="navbar-toggler-icon"></span>
+	</button>
+	<div class="collapse navbar-collapse" id="navbarResponsive">
+	  <ul class="navbar-nav ml-auto">
+		<li class="nav-item">
+		  <a class="nav-link" href="#">Reservations</a>
+		</li>
+		<li class="nav-item">
+		  <a class="nav-link" href="#">Jobs</a>
+		</li>
+		<li class="nav-item">
+		  <a class="nav-link" href="#"><i class="fa fa-star" aria-hidden="true"></i> Contact Us</a>
+		</li>
+		<li class="nav-item">
+		  <a class="nav-link" href="#" class="lead"><strong>1-800-123-4567</strong></a>
+		</li>
+	  </ul>
+	</div>
+  </div>
+</nav>
+
+<style>
+body {
+padding-top: 54px;
+}
+@media (min-width: 992px) {
+body {
+padding-top: 56px;
+	}
+}
+h3 {
+font: 300 30px/1.3 'Oleo Script', Helvetica, sans-serif;
+color: #2B2B2B;
+}
+.navbar-brand {
+font: 200 30px/1.3 'Oleo Script', Helvetica, sans-serif;
+color: #FFFFFF;
+}
+span {
+font: 200 17px/1.3 Helvetica, sans-serif;
+color: #2B2B2B;
+}
+.bg-light { background-color: #ED9121 !important; }
+a { color: #000000; }
+a:hover { color: #000000; }
+.nav-link { color: #000 !important; }
+</style>
+
+<link href='http://fonts.googleapis.com/css?family=Oleo+Script' rel='stylesheet' type='text/css'>
+
+<link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet">
+
+<!-- Page Content -->
+<div class="container text-center"  style="background-image: url(dinerfire.jpg height:1000px;width:750%);color:white;background-repeat: no-repeat;background-color:black;height:500px;width:4000%;">
+<div class="row">
+  <div class="col-lg-12 text-center">
+	<h1 class="mt-5" style="padding-top: 100px;">Voted Best New Restaurant in Jacksonville!</h1>
+	<p style="padding-top:100px;"><button type="button" class="btn btn-success">Order Online</button></p>
+	<br><br>
+  </div>
+</div>
+</div> <!--/container-->
+<div class="container" style="background-color:#FFA500;opacity: 0.9;">
+<div class="row">
+<div class="col-lg-12 text-center"><br>
+  <h3>Our Menus</h3><br>
+</div>
+ <br><br>
+<div class="col-lg-4 text-center">
+  <img src="breakfst.jpg" class="img-thumbnail">
+  <br><br><a href="#" class="btn btn-dark">Breakfast</a><br><br>
+</div>
+<div class="col-lg-4 text-center">
+  <img src="lunch.jpg" class="img-thumbnail">
+  <br><br><a href="#" class="btn btn-dark">Lunch</a><br><br>
+</div>
+<div class="col-lg-4 text-center">
+  <img src="dinner.jpg" class="img-thumbnail">
+  <br><br><a href="#" class="btn btn-dark">Dinner</a><br><br>
+</div>
+<br><br>
+</div> <!--/row-->
+<br><br>
+</div> <!--/container-->
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
+<div class="container" style="background-color:#ffffff;">
+<div class="row">
+<div class="col-lg-6"><br>
+<h3>Our Chef: Macy Cooke</h3><br>
+<p class="lead" style=""><img src="chef.jpg" class="img-thumbnail" style="padding:10px;width:150px;float:right;">Macy was born in Asheville, NC. She was raised in Boston, MA and attended the New England Culinary Institute.
+<br><br>
+If she’s not at <strong>Bistros</strong>, you’ll find her cheering on the  Boston Red Sox or spending time with her dog, Meka. She considers Julia Child her hero and her mother to be her greatest inspiration.
+<br><br>
+Macy has been working in the restaurant industry for a very long time and really enjoys the environment <strong>Bistros</strong> offers.<br>
+<br><img src="Socialicons.jpg"></p>
+</div>
+<div class="col-lg-6" style="background-color:#F8F8F8;border-left:1px dashed gray;"><br>
+<h3>Critical Acclaim</h3><br>
+<p class="lead" style="text-align: justify;"><i class="fas fa-star" style="color:gold;text-shadow: 1px 1px black;font-size:18px;"></i><i class="fas fa-star" style="color:gold;text-shadow: 1px 1px black;font-size:18px;"></i><i class="fas fa-star" style="color:gold;text-shadow: 1px 1px black;font-size:18px;"></i><i class="fas fa-star" style="color:gold;text-shadow: 1px 1px black;font-size:18px;"></i><i class="fas fa-star" style="color:gold;text-shadow: 1px 1px black;font-size:18px;"></i> "Ok, this is the third time I have had a meal at this restaurant and I can state every meal was excellent. From the garlic bread with house made dressings to pasta and veal dishes, it has always been excellent."<br><span style="color:#30a64a;font-size:14px;float:right;">--The Jacksonville Sun</span></p>
+<br><br>
+<p class="lead" style="text-align: justify;"><i class="fas fa-star" style="color:gold;text-shadow: 1px 1px black;font-size:18px;"></i><i class="fas fa-star" style="color:gold;text-shadow: 1px 1px black;font-size:18px;"></i><i class="fas fa-star" style="color:gold;text-shadow: 1px 1px black;font-size:18px;"></i><i class="fas fa-star" style="color:gold;text-shadow: 1px 1px black;font-size:18px;"></i><i class="fas fa-star" style="color:gold;text-shadow: 1px 1px black;font-size:18px;"></i> "Bistros might be the best restaurant you’ve never been to. Overlooking the water and just steps from a marina. Once you arrive, you can get lost again in a menu over loaded with all the classic comfort foods you love – like baked stuffed lobster, succulent surf and turf and an overloaded fisherman’s platter." <br><span style="color:#30a64a;font-size:14px;float:right;">--The Phantom Gourmet</span></p>
+<br><br>
+</div> <!--/col-->
+</div> <!--/row-->
+</div> <!--/container-->
+<div class="container" style="background-color:#FFA824;color:gray;opacity: 0.81;">
+<div class="row">
+<div class="col-lg-4">
+<p class="text-left" style="line-height:2.9em;color:#000000;padding-top:14px;font-size:14px;">&copy; Copyright 2018, Bistros LLC</p>
+</div>
+<div class="col-lg-4">
+<p class="text-center" style="line-height:2.9em;color:#000000;padding-top:14px;font-size:14px;">Bistros, 20 East Bay St., Jax, FL 32222</p>
+</div>
+<div class="col-lg-4">
+<p class="text-right" style="line-height:2.9em;color:#000000;padding-top:14px;font-size:14px;"><a href="#">Privacy Policy</a> | <a href="#">Terms of Use</a></p>
+</div>
+</div> <!--/row-->
+</div> <!--/container-->
